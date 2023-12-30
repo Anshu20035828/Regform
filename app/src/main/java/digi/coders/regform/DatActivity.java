265486b1c2f00b26cec75bee500f984dd24e2b05
ANSHUSINGH <23anshusingh2003@gmail.com>
2023-12-30 13:31:00 +0530

    Helio

---
+++ app/src/main/java/digi/coders/regform/DatActivity.java
@@ -0,0 +1,14 @@
+package digi.coders.regform;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dat);
+    }
+}
